caf3f6129ce0db5788332438c1dd13b166053498
Louis <louis.navone7@etu.univ-lorraine.fr>
2017-05-12 14:32:05 +0200

    Auto stash before merge of "master" and "origin/master"

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 
+
 session_start();
 require 'vendor/autoload.php';
 
-$app = new \Slim\Slim();
+$app = new \Slim\Slim ();
 
 \PPIL\utils\ConnectionFactory::setConfig('db.ppil.conf.ini');
 \PPIL\utils\ConnectionFactory::makeConnection();
