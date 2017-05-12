c3d8765e6ce659c4343cfe0a2edd3243602683ef
François Lallemand <francois.lallemand@openmailbox.org>
2017-05-12 15:00:52 +0200

    correction de index.php

--- index.php
+++ index.php
@@ -4,12 +4,18 @@
 session_start();
 require 'vendor/autoload.php';
 
-$app = new \Slim\Slim ();
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+$app = new \Slim\App;
 
 \PPIL\utils\ConnectionFactory::setConfig('db.ppil.conf.ini');
 \PPIL\utils\ConnectionFactory::makeConnection();
 
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write("Hello");
 
-
+    return $response;
+});
 
 $app->run();
