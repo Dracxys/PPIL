60c3d159e9915f8768c0a3058e9ff9a6bf039cea
Louis <louis.navone7@etu.univ-lorraine.fr>
2017-05-12 14:14:58 +0200

    fichiers importants

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+session_start();
+require 'vendor/autoload.php';
+
+$app = new \Slim\Slim();
+
+\PPIL\utils\ConnectionFactory::setConfig('db.ppil.conf.ini');
+\PPIL\utils\ConnectionFactory::makeConnection();
+
+
+
+
+$app->run();
