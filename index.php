85abb94bcaa19fee9a4a0e396453df2e3ee11929
Louis <louis.navone7@etu.univ-lorraine.fr>
2017-05-13 10:31:50 +0200

    Modification version de Slim

--- index.php
+++ index.php
@@ -4,11 +4,10 @@
 session_start();
 require 'vendor/autoload.php';
 
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
+
 use \PPIL\controlers\HomeControler as HomeControler;
 
-$app = new \Slim\App;
+$app = new \Slim\Slim();
 
 \PPIL\utils\ConnectionFactory::setConfig('db.ppil.conf.ini');
 \PPIL\utils\ConnectionFactory::makeConnection();
