22a5462caa594e131aba063cd9729b0d6e206329
François Lallemand <francois.lallemand@openmailbox.org>
2017-05-13 21:27:51 +0200

    récup du mail

--- index.php
+++ index.php
@@ -12,15 +12,18 @@ $app = new \Slim\Slim();
 \PPIL\utils\ConnectionFactory::setConfig('db.ppil.conf.ini');
 \PPIL\utils\ConnectionFactory::makeConnection();
 
-$app->get('/', function () {
+$app->get('/', function () use ($app) {
     $c = new HomeControler();
     $c->accueil();
 });
 
 
-$app->post('/login', function (){
-    $c = new HomeControler();
-    $c->connection();
+$app->post('/login', function () use ($app){
+    $request = $app->request();
+    $email = $request->post('email');
+    echo $email;
+    //    $c = new HomeControler();
+    //$c->connection();
 })->name('login');
 
 $app->run();
