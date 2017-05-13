cdc845f6e6c6f88e0ea832bc19c6464d8348b87c
François Lallemand <francois.lallemand@openmailbox.org>
2017-05-13 11:24:59 +0200

    ajout du squelette de UE

--- src/PPIL/models/Intervention.php
+++ src/PPIL/models/Intervention.php
@@ -1,7 +1,7 @@
 <?php
 namespace PPIL\models;
 
-class  Intervention extends \Illuminate\Database\Eloquent\Model{
+class Intervention extends \Illuminate\Database\Eloquent\Model{
 	protected $table = "Intervention";
 	protected $primaryKey = "id_intervention";
 	public $timestamps = false;
---
+++ src/PPIL/models/UE.php
@@ -0,0 +1,9 @@
+<?php
+namespace PPIL\models;
+
+class UE extends \Illuminate\Database\Eloquent\Model{
+	protected $table = "UE";
+	protected $primaryKey = "nom_UE";
+	public $timestamps = false;
+
+}
