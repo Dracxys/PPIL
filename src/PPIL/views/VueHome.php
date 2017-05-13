c3da9a84d7f3df1a96f3ff7a670812ac0940d064
François Lallemand <francois.lallemand@openmailbox.org>
2017-05-13 16:13:05 +0200

    ajouts pour utiliser bootstrap - 2

--- src/PPIL/views/VueHome.php
+++ src/PPIL/views/VueHome.php
@@ -19,18 +19,17 @@ class VueHome extends AbstractView
         $lien = Slim::getInstance()->urlFor("login");
         $html = $html . <<< END
         <div class="container">
-        <form method="post" action="$lien" id="connexion">
+          <form method="post" action="$lien" id="connexion">
             <div class="form-group">
-                <label for="email">Adresse Email :</label>
-                <input type="email" id="fieldEmail" class="form-control" placeholder="Adresse Mail" />
+              <label for="email">Adresse Email :</label>
+              <input type="email" id="fieldEmail" class="form-control" placeholder="Adresse Mail" />
             </div>
             <div class="form-group">
-                <label for="password">Mot de passe :</label>
-                <input type="password" id="fieldPassword" class="form-control" placeholder="Mot de passe" />
+              <label for="password">Mot de passe :</label>
+              <input type="password" id="fieldPassword" class="form-control" placeholder="Mot de passe" />
             </div>
-                <button type="submit" class="btn btn-default">Connexion</button>
-            </form>
-        </div>
+            <button type="submit" class="btn btn-default">Connexion</button>
+          </form>
         </div>
 END;
         return $html;
