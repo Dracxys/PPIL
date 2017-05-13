bab8fd4728732b5142b347152bb40b620d985cd8
François Lallemand <francois.lallemand@openmailbox.org>
2017-05-13 11:19:42 +0200

    ajout du squelette de Intervention

---
+++ src/PPIL/models/Intervention.php
@@ -0,0 +1,9 @@
+<?php
+namespace PPIL\models;
+
+class  Intervention extends \Illuminate\Database\Eloquent\Model{
+	protected $table = "Intervention";
+	protected $primaryKey = "id_intervention";
+	public $timestamps = false;
+
+}
