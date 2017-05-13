a2c4f2d9c44544d8f742e0248cdff1f144b85c3d
François Lallemand <francois.lallemand@openmailbox.org>
2017-05-13 17:23:41 +0200

    Ajout du header

--- src/PPIL/views/AbstractView.php
+++ src/PPIL/views/AbstractView.php
@@ -18,6 +18,21 @@ class AbstractView {
     <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+  <div class="jumbotron">
+	<div class="container">
+	<div class="row">
+      <div class="hidden-xs col-sm-2">
+            <div class="logo-univ">
+                <img width="100" height="100" src="assets/images/logo-univ.png" />
+            </div>
+        </div>
+        <div class="col-sm-10">
+		  <h1>Service enseignant</h1>
+        </div>
+    </div>
+	</div>
+  </div>
+
 END;
         return $HTML;
     }
