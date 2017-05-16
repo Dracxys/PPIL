687854c77737e4b49612f31b163e1659211cd824
François Lallemand <francois.lallemand@openmailbox.org>
2017-05-16 18:06:20 +0200

    correction

--- src/PPIL/models/Responsabilite.php
+++ src/PPIL/models/Responsabilite.php
@@ -6,19 +6,19 @@ class Responsabilite extends \Illuminate\Database\Eloquent\Model{
 	protected $primaryKey = "id_resp";
 	public $timestamps = false;
 
-	public static function ajoutResponsabilite($mail, $intitule_responsabilite, $id_formation, $id_UE){		
+	public static function ajoutResponsabilite($mail, $intitule_responsabilite, $id_formation, $id_UE){
 		$n = new Responsabilite();
 		$n->intituleResp = strtolower ($intitule_responsabilite);
 		$n->enseignant = $mail;
-		
+
 		if(strcmp(strtolower ($intitule_responsabilite), 'responsable ue')){
 			$n->id_UE = $id_UE;
-			$n->priorite = 1
+			$n->priorite = 0;
 		} else if(strcmp(strtolower ($intitule_responsabilite), 'responsable formation')){
 			$n->id_formation = $id_formation;
-			$n->priorite = 2;
+			$n->priorite = 1;
 		} else {
-			$n->priorite = 3;
+			$n->priorite = 2;
 		}
 		$n->save();
 	}
